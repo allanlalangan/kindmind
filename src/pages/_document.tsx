844b3a127a5fe63030dbb602d9a0f9d6835a828d
allanlalangan <allanlalangan@gmail.com>
2023-07-19 23:11:28 -0700

    import and update fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="dark" lang="en">
       <Head />
-      <body className="bg-gray-900 text-gray-50">
+      <body className="bg-amber-50 text-gray-900 dark:bg-gray-900 dark:text-gray-50">
         <Main />
         <NextScript />
       </body>
