7b4863a26f28d227f1ff18e8179dbd59f45904b6
allanlalangan <allanlalangan@gmail.com>
2023-08-29 21:05:29 -0700

    update sidebar links

--- src/components/DashboardLayout/index.tsx
+++ src/components/DashboardLayout/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ThemeSwitch from "../ThemeSwitch";
+import Link from "next/link";
 
 export default function DashboardLayout({
   children,
@@ -10,7 +11,7 @@ export default function DashboardLayout({
   return (
     <>
       <div className="grid h-screen grid-cols-12 grid-rows-24">
-        <header className="col-span-12 flex h-full items-center justify-between border-b border-light-500 bg-light-300 px-4 dark:border-b dark:border-base-800 dark:bg-base-900 lg:col-start-3 lg:col-end-13 lg:justify-end">
+        <header className="col-span-12 flex h-full items-center justify-between border-b border-light-500 bg-light-300 px-4 transition dark:border-b dark:border-base-800 dark:bg-base-900 lg:col-start-3 lg:col-end-13 lg:justify-end">
           <button
             onClick={() => setNavIsOpen(!navIsOpen)}
             className="rounded-lg border border-primary-400 bg-light-300 p-1 text-primary-500 transition-colors hover:border-primary-500 hover:bg-light-400 active:border-primary-600 active:bg-light-500 dark:border-tertiary-200 dark:bg-base-900 dark:text-tertiary-300 dark:hover:border-tertiary-300 dark:hover:bg-base-800 dark:active:border-tertiary-400 dark:active:bg-base-900 lg:hidden"
@@ -38,13 +39,37 @@ export default function DashboardLayout({
             !navIsOpen ? "-translate-x-full" : ""
           } fixed row-span-full row-start-1 h-screen w-1/2 bg-light-400 px-4 py-2 transition dark:bg-base-950 md:w-1/3 lg:static lg:col-start-1 lg:col-end-3 lg:w-full lg:-translate-x-0`}
         >
-          <span className="font-display text-3xl uppercase tracking-wide text-primary-600 dark:text-tertiary-300">
+          <Link
+            href="/"
+            className="font-display text-3xl uppercase tracking-wide text-primary-600 dark:text-tertiary-300"
+          >
             Stars
-          </span>
+          </Link>
           <ul className="mt-4">
-            <li>Link 1</li>
-            <li>Link 2</li>
-            <li>Link 3</li>
+            <li>
+              <Link
+                href="/dashboard"
+                className="underline-offset-2 hover:text-primary-600 hover:underline dark:hover:text-tertiary-300"
+              >
+                Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/journal"
+                className="underline-offset-2 hover:text-primary-600 hover:underline dark:hover:text-tertiary-300"
+              >
+                Journal
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/dashboard/charts"
+                className="underline-offset-2 hover:text-primary-600 hover:underline dark:hover:text-tertiary-300"
+              >
+                Charts
+              </Link>
+            </li>
           </ul>
           <button
             onClick={() => setNavIsOpen(!navIsOpen)}
@@ -66,9 +91,6 @@ export default function DashboardLayout({
           </button>
         </aside>
         <main className="col-span-12 row-span-full row-start-2 flex flex-col p-4 lg:col-start-3 lg:col-end-13">
-          <h1 className="font-serif text-xl dark:text-primary-300">
-            Dashboard
-          </h1>
           {children}
         </main>
       </div>
