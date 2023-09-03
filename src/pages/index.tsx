02a39bdde4e260d84a9a488c82cc10324543bd57
allanlalangan <allanlalangan@gmail.com>
2023-09-03 11:48:41 -0700

    update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,6 @@ import ThemeSwitch from "~/components/ThemeSwitch";
 
 export default function Home() {
   const user = useUser();
-  const moon_phase_report = {
-    considered_date: "2-10-2017",
-    moon_phase: "Balsamic Moon",
-    significance:
-      "The Moon now is waning and is located halfway between Last Quarter and New Moon. The seeds are moving towards maturation, while rest of the plant withers away. The energy of the plant concentrates into the seed which is preparing itself for the next cycle. In the seasonal progression, this phase represents Halloween, midway between the Fall Equinox and the Winter Solstice. The wall that separates the dead from the living, thins and ghosts walk on the plant of the living.",
-    report:
-      "This is a time to rest, recreate and reflect on yourself and try to reconnect with inner powerhouses of strength. It is currently not a good time to start something afresh. It is a phase where all you should do is reflect your life objectively and increase awareness of what is important to you. At this point, make your bucket list and realise your dreams and aspirations. What are the larger goals that you can you aspire to and will bring vitality and excitement into your life and make your life a little more interesting and offbeat? Just think and reflect on those aspects. Do not jump on the implementation of the listed goals.",
-  };
   return (
     <>
       <Head>
@@ -75,24 +67,14 @@ export default function Home() {
                 </SignOutButton>
               </>
             )}
-            <Link className="underline underline-offset-4" href="/dashboard">
+            <Link
+              className="w-fit rounded bg-secondary-400 px-4 pb-2.5 pt-2 text-base-50 transition-colors hover:bg-secondary-500 hover:underline active:bg-secondary-600 dark:bg-primary-600 dark:hover:bg-primary-700 dark:active:bg-primary-800"
+              href="/dashboard"
+            >
               Dashboard
             </Link>
           </section>
         </section>
-
-        <article className="mx-4 rounded bg-light-200 p-8 dark:bg-base-800 md:mx-36 lg:mx-72">
-          <h2 className="mb-4 text-lg font-bold text-primary-600 dark:text-tertiary-200">
-            Sample Moon Report
-          </h2>
-          <p className="mb-2">Date: {moon_phase_report.considered_date}</p>
-          <p className="mb-2">Moon Phase: {moon_phase_report.moon_phase}</p>
-          <p className="mb-2">{moon_phase_report.significance} </p>
-          <p className="mb-2">{moon_phase_report.report}</p>
-          <button className="mt-4 w-fit rounded bg-secondary-400 px-4 pb-2.5 pt-2 text-base-50 transition-colors hover:bg-secondary-500 active:bg-secondary-600 dark:bg-primary-600 dark:hover:bg-primary-700 dark:active:bg-primary-800">
-            Generate Journal Prompts (work in progress)
-          </button>
-        </article>
       </main>
     </>
   );
