5bd2ada596304f4c25ec01ee133c4ece2f5f1ecd
allanlalangan <allanlalangan@gmail.com>
2023-09-11 21:22:34 -0700

    update dashboardlayout sidebar position

--- src/components/DashboardLayout/index.tsx
+++ src/components/DashboardLayout/index.tsx
@@ -37,7 +37,7 @@ export default function DashboardLayout({
         <aside
           className={`${
             !navIsOpen ? "-translate-x-full" : ""
-          } fixed z-10 row-span-full row-start-1 h-screen w-1/2 bg-light-400 px-4 py-2 transition dark:bg-base-950 md:w-1/3 lg:static lg:col-start-1 lg:col-end-3 lg:w-full lg:-translate-x-0`}
+          } fixed z-10 row-span-full row-start-1 h-screen w-1/2 bg-light-400 px-4 py-2 transition dark:bg-base-950 md:w-1/6 lg:col-start-1 lg:col-end-3 lg:-translate-x-0`}
         >
           <Link
             onClick={() => setNavIsOpen(false)}
