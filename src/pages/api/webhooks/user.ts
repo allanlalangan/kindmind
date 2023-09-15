1aa4425f4c729c06402ee6d14580e119374b56d3
allanlalangan <allanlalangan@gmail.com>
2023-09-14 21:12:41 -0700

    anotha one

--- src/pages/api/webhooks/user.ts
+++ src/pages/api/webhooks/user.ts
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { Webhook, type WebhookRequiredHeaders } from "svix";
 import type { WebhookEvent } from "@clerk/nextjs/server";
 import { prisma } from "~/server/db";
+import { headers } from "next/headers";
 
 const webhookSecret: string = process.env.WEBHOOK_SECRET!;
 
@@ -15,14 +16,25 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const payload = JSON.stringify(await req.body);
-  const headers = req.headers;
+
+  const headersList = headers();
+  const svix = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+
+  // const headers = req.headers;
   // Create a new Webhook instance with your webhook secret
   const wh = new Webhook(webhookSecret);
 
   let evt: WebhookEvent;
   try {
     // Verify the webhook payload and headers
-    evt = (await wh.verify(payload, headers)) as WebhookEvent;
+    evt = (await wh.verify(
+      payload,
+      svix as IncomingHttpHeaders & WebhookRequiredHeaders
+    )) as WebhookEvent;
   } catch (error) {
     // If the verification fails, return a 400 error
     return res.status(400).json({ error, payload, headers });
