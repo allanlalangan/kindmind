36fa87d617427e73f9b0592c4a33541e3bcad9f2
allanlalangan <allanlalangan@gmail.com>
2023-09-14 19:12:00 -0700

    feat: update webhook

--- src/pages/api/webhooks/user.ts
+++ src/pages/api/webhooks/user.ts
@@ -3,7 +3,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import type { WebhookRequiredHeaders } from "svix";
 import type { WebhookEvent } from "@clerk/nextjs/server";
 import { Webhook } from "svix";
-import { buffer } from "micro";
 import { prisma } from "~/server/db";
 
 const webhookSecret: string = process.env.WEBHOOK_SECRET!;
@@ -12,7 +11,7 @@ export default async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
-  const payload = (await buffer(req)).toString();
+  const payload = JSON.stringify(req.body);
   const headers = req.headers;
   // Create a new Webhook instance with your webhook secret
   const wh = new Webhook(webhookSecret);
@@ -25,10 +24,10 @@ export default async function handler(
     // If the verification fails, return a 400 error
     return res.status(400).json({});
   }
-  const { id } = evt.data;
 
   const eventType = evt.type;
   if (eventType === "user.created" || eventType === "user.updated") {
+    const { id } = evt.data;
     console.log(`User ${id} was ${eventType}`);
     await prisma.user.upsert({
       where: {
@@ -36,13 +35,14 @@ export default async function handler(
       },
       update: {},
       create: {
-        clerkId: id!,
+        clerkId: id,
       },
     });
     res.status(200).json({});
   }
 
   if (eventType === "user.deleted") {
+    const { id } = evt.data;
     await prisma.user.delete({
       where: {
         clerkId: id,
