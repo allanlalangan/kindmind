5026ffb665604f2108c14336d596128714a6ad8a
allanlalangan <allanlalangan@gmail.com>
2023-09-14 19:49:48 -0700

    wtf

--- src/pages/api/webhooks/user.ts
+++ src/pages/api/webhooks/user.ts
@@ -1,41 +1,47 @@
-import type { IncomingHttpHeaders } from "http";
+import { type IncomingHttpHeaders } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
-import type { WebhookRequiredHeaders } from "svix";
-import type { WebhookEvent } from "@clerk/nextjs/server";
-import { Webhook } from "svix";
+import { Webhook, type WebhookRequiredHeaders } from "svix";
+import { buffer } from "micro";
 import { prisma } from "~/server/db";
 
-const webhookSecret: string = process.env.WEBHOOK_SECRET!;
+// Disable the bodyParser so we can access the raw
+// request body for verification.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret: string = process.env.WEBHOOK_SECRET ?? "";
 
 export default async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
-  const payload = JSON.stringify(req.body);
+  // Verify the webhook signature
+  // See https://docs.svix.com/receiving/verifying-payloads/how
+  const payload = (await buffer(req)).toString();
   const headers = req.headers;
-  // Create a new Webhook instance with your webhook secret
   const wh = new Webhook(webhookSecret);
-
-  let evt: WebhookEvent;
+  let evt: Event | null = null;
   try {
-    // Verify the webhook payload and headers
-    evt = wh.verify(payload, headers) as WebhookEvent;
+    evt = wh.verify(payload, headers) as Event;
   } catch (_) {
-    // If the verification fails, return a 400 error
     return res.status(400).json({});
   }
 
-  const eventType = evt.type;
+  // Handle the webhook
+  const eventType: EventType = evt.type;
   if (eventType === "user.created" || eventType === "user.updated") {
     const { id } = evt.data;
     console.log(`User ${id} was ${eventType}`);
     await prisma.user.upsert({
       where: {
-        clerkId: id,
+        clerkId: id as string,
       },
       update: {},
       create: {
-        clerkId: id,
+        clerkId: id as string,
       },
     });
     res.status(200).json({});
@@ -45,13 +51,25 @@ export default async function handler(
     const { id } = evt.data;
     await prisma.user.delete({
       where: {
-        clerkId: id,
+        clerkId: id as string,
       },
     });
     res.status(200).json({});
   }
+
+  res.json({});
 }
 
 type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
 };
+
+// Generic (and naive) way for the Clerk event
+// payload type.
+interface Event {
+  data: Record<string, string | number>;
+  object: "event";
+  type: EventType;
+}
+
+type EventType = "user.created" | "user.updated" | "user.deleted" | "*";
