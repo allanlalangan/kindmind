9c04da78f47f1d3f697cb84a8afec9f1a110a484
allanlalangan <allanlalangan@gmail.com>
2023-10-03 19:39:05 -0700

    feat: create HealthSelector

---
+++ src/lib/event_selectors.ts
@@ -0,0 +1,14 @@
+const self_care = ["exercise", "journaling", "grooming", "meditate"];
+
+const activities = [
+  "art",
+  "music",
+  "tv",
+  "games",
+  "reading",
+  "code",
+  "shopping",
+  "other",
+];
+
+const work = ["job", "study", "clean", "errands"];
